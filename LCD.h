ac2a6eaff49a766ad2564173970b83ba731a9668
T1t4n25 <54239400+T1t4n25@users.noreply.github.com>
2024-08-26 17:22:15 +0300

    Add files via upload

---
+++ LCD.h
@@ -0,0 +1,44 @@
+﻿/*
+ * LCD.h
+ *
+ * Created: 16/08/2024 12:35:46 م
+ *  Author: Meteor
+ */ 
+
+
+#ifndef LCD_H_
+#define LCD_H_
+
+#define EIGHT_BIT_2LINE_SMALL 0x38
+#define FOUR_BIT_2LINE_SMALL 0x20
+#define EIGHT_BIT_1LINE_SMALL 0x30
+#define FOUR_BIT_1LINE_SMALL 0x28
+#define ENTRY_MODE_DEC_CURSOR 0x04
+#define ENTRY_MODE_INC_CURSOR 0x06
+#define ENTRY_MODE_SHIFT_RIGHT 0x05
+#define ENTRY_MODE_SHIFT_LEFT 0x07
+#define DISPLAY_OFF_CURSOR_OFF 0x08
+#define DISPLAY_OFF_CURSOR_ON 0x0A
+#define DISPLAY_ON_CURSOR_OFF 0x0C
+#define DISPLAY_ON_CURSOR_BLINK 0x0F
+#define DISPLAY_ON_CURSOR_ON 0x0E
+#define SHIFT_CURSOR_LEFT 0x10
+#define SHIFT_CURSOR_RIGHT 0x14
+#define SHIFT_DISPLAY_LEFT 0x18
+#define SHIFT_DISPLAY_RIGHT 0x1C
+#define FORCE_RETURN_FIRST_LINE 0x80
+#define FORCE_RETURN_SECOND_LINE 0xC0
+#define CLEAR_DISPLAY 0x01
+#define RETURN_HOME 0x02
+#define MAX_ROW 2
+#define MAX_COL 16
+
+
+void SEND_COMMAND(char instruction);
+void LCD_init(void); // returns 1 when completed
+void LCD_WRITE_CHAR(char character);
+void LCD_WRITE_STRING(char string[]);
+void LCD_CURSOR_MOVE(unsigned char line, unsigned char index);
+
+#endif /* LCD_H_ */
+
---
+++ main.c
@@ -0,0 +1,71 @@
+/*
+ * LCD Driver.c
+ *
+ * Created: 16/08/2024 12:32:29 PM
+ * Author : Meteor
+ */ 
+#define F_CPU 8000000UL
+#include <avr/io.h>
+#include "LCD.h"
+#include "conf.h"
+#include<util/delay.h>
+
+void intToStr(char N, char *str);
+
+
+int main(void)
+{
+	DDRC = 0xFF;
+	DDRD = 0xFF;
+	LCD_init();
+	
+	char number[4];
+	char FirstRow[4] = " = ";
+	for(unsigned char i = 0; i < 255; i++){
+		intToStr(i, number);
+		LCD_WRITE_STRING(number);
+		LCD_WRITE_STRING(FirstRow);
+		LCD_WRITE_CHAR(i);
+		_delay_ms(10);
+		SEND_COMMAND(CLEAR_DISPLAY);
+		SEND_COMMAND(RETURN_HOME);
+	}
+	
+}
+
+void intToStr(char N, char *str) {
+	int i = 0;
+	
+	// Save the copy of the number for sign
+	char sign = N;
+
+	// If the number is negative, make it positive
+	if (N < 0)
+	N = -N;
+
+	// Extract digits from the number and add them to the
+	// string
+	while (N > 0) {
+		
+		// Convert integer digit to character and store
+		// it in the str
+		str[i++] = N % 10 + '0';
+		N /= 10;
+	}
+
+	// If the number was negative, add a minus sign to the
+	// string
+	if (sign < 0) {
+		str[i++] = '-';
+	}
+
+	// Null-terminate the string
+	str[i] = '\0';
+
+	// Reverse the string to get the correct order
+	for (int j = 0, k = i - 1; j < k; j++, k--) {
+		char temp = str[j];
+		str[j] = str[k];
+		str[k] = temp;
+	}
+}
