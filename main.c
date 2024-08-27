fefa4663250c6468f212ffab158ae6da59d8d0b2
T1t4n25 <54239400+T1t4n25@users.noreply.github.com>
2024-08-27 17:27:27 +0300

    Update main.c

--- main.c
+++ main.c
@@ -18,54 +18,7 @@ int main(void)
 	DDRC = 0xFF;
 	DDRD = 0xFF;
 	LCD_init();
-	
-	char number[4];
-	char FirstRow[4] = " = ";
-	for(unsigned char i = 0; i < 255; i++){
-		intToStr(i, number);
-		LCD_WRITE_STRING(number);
-		LCD_WRITE_STRING(FirstRow);
-		LCD_WRITE_CHAR(i);
-		_delay_ms(10);
-		SEND_COMMAND(CLEAR_DISPLAY);
-		SEND_COMMAND(RETURN_HOME);
-	}
+	// WRITE APP HERE
 	
 }
 
-void intToStr(char N, char *str) {
-	int i = 0;
-	
-	// Save the copy of the number for sign
-	char sign = N;
-
-	// If the number is negative, make it positive
-	if (N < 0)
-	N = -N;
-
-	// Extract digits from the number and add them to the
-	// string
-	while (N > 0) {
-		
-		// Convert integer digit to character and store
-		// it in the str
-		str[i++] = N % 10 + '0';
-		N /= 10;
-	}
-
-	// If the number was negative, add a minus sign to the
-	// string
-	if (sign < 0) {
-		str[i++] = '-';
-	}
-
-	// Null-terminate the string
-	str[i] = '\0';
-
-	// Reverse the string to get the correct order
-	for (int j = 0, k = i - 1; j < k; j++, k--) {
-		char temp = str[j];
-		str[j] = str[k];
-		str[k] = temp;
-	}
-}
