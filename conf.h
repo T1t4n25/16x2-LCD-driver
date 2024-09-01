3eec5a0d3385a6e8dc45520955beca5a9df0e737
T1t4n25 <54239400+T1t4n25@users.noreply.github.com>
2024-09-01 15:30:22 +0300

    Moved MCAL include from lcd.c to conf.h

--- conf.h
+++ conf.h
@@ -8,6 +8,8 @@
 
 #ifndef CONF_H_
 #define CONF_H_
+// include the MCAL library 
+#include <avr/io.h>
 
 #define FOUR_BIT 4
 #define EIGHT_BIT 8
@@ -23,8 +25,8 @@
 // Set to 1 to get errors if initialization in missed - 0 to ignore errors
 // it is preferred to leave it = 1 if you don't know how to initialize LCDs
 #define COMPILER_ERRORS 1
-// limits the string write length to MAX_COL size which is 16 for 16*2 displays
-// making it 0 will result in ignoring the limit
+// Set to 1 limits the string write length and LCD_CURSOR_MOVE() to MAX_COL size which is 16 for 16*2 displays
+// Set to 0 will result in ignoring the limit
 #define LIMIT_WRITES 1
 
 #endif /* CONF_H_ */
